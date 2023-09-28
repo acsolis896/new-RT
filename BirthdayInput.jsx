e4e97037fb301481f2aa308372dcf31160952948
Andrea Solis <acsolis896@gmail.com>
2023-09-27 23:11:32 -0400

    css

--- BirthdayInput.jsx
+++ BirthdayInput.jsx
@@ -13,40 +13,53 @@ function BirthdayInput() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <label for="month">Month</label>
-        <input 
-          class="bday-input dark-grey-bg"
-          id="month"
-          type="text"
-          placeholder="MM" 
-          value={month} 
-          onChange={(e) => setMonth(e.target.value)}
-          min="1"
-          max="12"
-          required
-        />
-      <label for="day">Day</label>
-        <input 
-          class="bday-input dark-grey-bg"
-          id="day"
-          type="text"
-          placeholder="DD"  
-          value={day} 
-          onChange={(e) => setDay(e.target.value)}
-          min="1"
-          max="31"
-          required
-        />
-      <label for="year">Year</label>
-        <input 
-          class="bday-input dark-grey-bg"
-          id="year"
-          type="text" 
-          placeholder="YYYY" 
-          value={year} 
-          onChange={(e) => setYear(e.target.value)}
-          required
-        />
+        <div class="input-containers">
+            <div>
+                <label for="month">Month</label>
+                <input 
+                class="bday-input dark-grey-bg"
+                id="month"
+                type="text"
+                placeholder="MM" 
+                value={month} 
+                onChange={(e) => setMonth(e.target.value)}
+                min="1"
+                max="12"
+                required
+                />
+            </div>
+
+            <div>
+                <label for="day">Day</label>
+                <input 
+                class="bday-input dark-grey-bg"
+                id="day"
+                type="text"
+                placeholder="DD"  
+                value={day} 
+                onChange={(e) => setDay(e.target.value)}
+                min="1"
+                max="31"
+                required
+                />
+            </div>
+
+            <div>
+                <label for="year">Year</label>
+                <input 
+                class="bday-input dark-grey-bg"
+                id="year"
+                type="text" 
+                placeholder="YYYY" 
+                value={year} 
+                onChange={(e) => setYear(e.target.value)}
+                required
+                />
+            </div>
+        </div>
+      
+      
+      
       <button type="submit">Submit</button>
     </form>
   );
