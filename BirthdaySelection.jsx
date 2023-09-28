0e02884339a6803f00d0fc6ae9146f2078ade8d8
Andrea Solis <acsolis896@gmail.com>
2023-09-27 22:28:54 -0400

    added image

--- BirthdaySelection.jsx
+++ BirthdaySelection.jsx
@@ -20,7 +20,7 @@ function BirthdaySelection() {
         <div class="bday-movies-div">
             <p>We love our birthday movies 🎉 </p>
             <div class="bday-movies-posters">
-                <img src="images/princess-diaries.png" alt="Princess Diaries movie poster"/>
+                <img src="images/princess-diaries.jpeg" alt="Princess Diaries movie poster"/>
                 <img src="images/13-going-on-30.png" alt="13 Going On 30 movie poster"/>
                 <img src="images/thisis-40.png" alt="This Is 40 movie poster"/>
                 <img src="images/17-again.png" alt="17 Again movie poster"/>
