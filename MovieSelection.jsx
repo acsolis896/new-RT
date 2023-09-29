5b117eebb7415f0adaef48c7e866cb5b36b68ecf
Andrea Solis <acsolis896@gmail.com>
2023-09-28 23:27:56 -0400

    changes

--- MovieSelection.jsx
+++ MovieSelection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function MovieSelection() {
-  const [topMovies, setTopMovies] = useState(['', '', '', '', '']);
+  const [topMovies, setTopMovies] = useState(['', '', '', '', '', '', '', '']);
   const [searchQuery, setSearchQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
 
@@ -31,12 +31,16 @@ function MovieSelection() {
     }
   };
 
-  // Integrate Firebase to save the top 5 movies
+  const removeMovieFromList = (index) => {
+    const newMovies = [...topMovies];
+    newMovies[index] = '';
+    setTopMovies(newMovies);
+  };
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <h2>Select your top 5 movies</h2>
-      <p>Selecting your top 5 movies will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
+      <h2>Select your top 8 movies</h2>
+      <p>Selecting your top 8 movies will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
 
       <input
         type="text"
@@ -52,15 +56,19 @@ function MovieSelection() {
         ))}
       </div>
 
-      {topMovies.map((movie, index) => (
-        <input
-          key={index}
-          type="text"
-          placeholder={`Movie ${index + 1}`}
-          value={movie}
-          onChange={(e) => handleMovieChange(e, index)}
-        />
-      ))}
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '16px', marginTop: '16px' }}>
+        {topMovies.map((movie, index) => (
+          <div key={index} style={{ position: 'relative' }}>
+            {movie ? 
+              <>
+                <img src="https://via.placeholder.com/100x150" alt={movie} style={{ width: '100%' }} />
+                <button onClick={() => removeMovieFromList(index)} style={{ position: 'absolute', right: 0, top: 0, background: 'red', color: 'white' }}>X</button>
+              </> : 
+              <div style={{ width: '100px', height: '150px', border: '1px solid gray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>Empty</div>
+            }
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
