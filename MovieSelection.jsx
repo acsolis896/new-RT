92f43a73cfca41374506be863da84cc950f86d02
Andrea Solis <acsolis896@gmail.com>
2023-09-28 23:34:41 -0400

    movie select

--- MovieSelection.jsx
+++ MovieSelection.jsx
@@ -1,18 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 function MovieSelection() {
   const [topMovies, setTopMovies] = useState(['', '', '', '', '', '', '', '']);
   const [searchQuery, setSearchQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const searchRef = useRef(null); // Reference to the search input
 
-  // Hardcoded movies for demonstration purposes
   const allMovies = ['The Shawshank Redemption', 'The Godfather', 'Pulp Fiction', 'Inception', 'The Dark Knight', 'Titanic', 'Forrest Gump'];
-
-  const handleMovieChange = (e, index) => {
-    const newMovies = [...topMovies];
-    newMovies[index] = e.target.value;
-    setTopMovies(newMovies);
-  };
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
@@ -38,23 +32,35 @@ function MovieSelection() {
   };
 
   return (
-    <div style={{ textAlign: 'center' }}>
+    <div style={{ textAlign: 'center', position: 'relative' }}>
       <h2>Select your top 8 movies</h2>
       <p>Selecting your top 8 movies will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
 
       <input
+        ref={searchRef}
         type="text"
         placeholder="Search for a movie..."
         value={searchQuery}
         onChange={handleSearchChange}
       />
-      <div>
-        {suggestions.map((suggestion, index) => (
-          <div key={index} onClick={() => addMovieToList(suggestion)}>
-            {suggestion}
-          </div>
-        ))}
-      </div>
+      {suggestions.length > 0 && (
+        <div style={{
+          position: 'absolute',
+          top: '100%',
+          left: 0,
+          width: '100%',
+          backgroundColor: 'white',
+          border: '1px solid gray',
+          zIndex: 1,
+          boxShadow: '0px 4px 6px rgba(0,0,0,0.1)'
+        }}>
+          {suggestions.map((suggestion, index) => (
+            <div key={index} onClick={() => addMovieToList(suggestion)} style={{ padding: '10px', cursor: 'pointer', borderBottom: '1px solid #eee' }}>
+              {suggestion}
+            </div>
+          ))}
+        </div>
+      )}
 
       <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr', gap: '16px', marginTop: '16px' }}>
         {topMovies.map((movie, index) => (
@@ -74,5 +80,3 @@ function MovieSelection() {
 }
 
 export default MovieSelection;
-
-
