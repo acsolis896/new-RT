a0389faf77ba4c9d83ad95d8e69ac79107261749
Andrea Solis <acsolis896@gmail.com>
2023-09-28 22:48:50 -0400

    movie selection

--- MovieSelection.jsx
+++ MovieSelection.jsx
@@ -2,11 +2,33 @@ import React, { useState } from 'react';
 
 function MovieSelection() {
   const [topMovies, setTopMovies] = useState(['', '', '', '', '']);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+
+  // Hardcoded movies for demonstration purposes
+  const allMovies = ['The Shawshank Redemption', 'The Godfather', 'Pulp Fiction', 'Inception', 'The Dark Knight', 'Titanic', 'Forrest Gump'];
 
   const handleMovieChange = (e, index) => {
     const newMovies = [...topMovies];
     newMovies[index] = e.target.value;
     setTopMovies(newMovies);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+    const searchResults = allMovies.filter(movie => movie.toLowerCase().includes(e.target.value.toLowerCase()));
+    setSuggestions(searchResults);
+  };
+
+  const addMovieToList = (movie) => {
+    const firstEmptyIndex = topMovies.indexOf('');
+    if (firstEmptyIndex !== -1) {
+      const newMovies = [...topMovies];
+      newMovies[firstEmptyIndex] = movie;
+      setTopMovies(newMovies);
+      setSearchQuery('');
+      setSuggestions([]);
+    }
   };
 
   // Integrate Firebase to save the top 5 movies
@@ -15,6 +37,21 @@ function MovieSelection() {
     <div style={{ textAlign: 'center' }}>
       <h2>Select your top 5 movies</h2>
       <p>Selecting your top 5 movies will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
+
+      <input
+        type="text"
+        placeholder="Search for a movie..."
+        value={searchQuery}
+        onChange={handleSearchChange}
+      />
+      <div>
+        {suggestions.map((suggestion, index) => (
+          <div key={index} onClick={() => addMovieToList(suggestion)}>
+            {suggestion}
+          </div>
+        ))}
+      </div>
+
       {topMovies.map((movie, index) => (
         <input
           key={index}
