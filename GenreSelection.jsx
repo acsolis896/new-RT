f70fdb8947e805ab19c3cf24dcfbe8afbf622bbf
Andrea Solis <acsolis896@gmail.com>
2023-09-29 15:50:47 -0400

    css

--- GenreSelection.jsx
+++ GenreSelection.jsx
@@ -29,7 +29,7 @@ function GenreSelection({ selectedGenres, setSelectedGenres }) {
 
   return (
     <div style={{marginTop: '30px'}}>
-      <h2 style={{textAlign: 'center', paddingBottom:'30px'}}>Select your top 3 genres for movies and TV</h2>
+      <h2 style={{textAlign: 'center', paddingBottom:'20px'}}>Select your top 3 genres for movies and TV</h2>
       <div className="genres-grid">
             {displayedGenres.map(genre => (
                 <div className={`genre-div ${selectedGenres.includes(genre) ? 'selected' : ''}`}>
