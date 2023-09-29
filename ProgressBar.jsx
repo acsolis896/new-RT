3dbd64e0f27f2aabe7f33f0e81044556dce755e1
Andrea Solis <acsolis896@gmail.com>
2023-09-29 14:52:05 -0400

    prg

--- ProgressBar.jsx
+++ ProgressBar.jsx
@@ -3,9 +3,9 @@ import './style.css';
 
 const ProgressBar = ({ currentStep, totalSteps = 5 }) => {
     return (
-        <div>
+        <div className="progress-bar">
             {Array.from({ length: totalSteps }).map((_, index) => (
-                <div className="progress-bar" key={index} className={`dot ${index < currentStep ? 'completed' : ''}`}></div>
+                <div key={index} className={`dot ${index < currentStep ? 'completed' : ''}`}></div>
             ))}
         </div>
     );
