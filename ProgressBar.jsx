4b1b793f66419eb30414094c9b22d685b7026b45
Andrea Solis <acsolis896@gmail.com>
2023-09-29 17:18:24 -0400

    inner circle

--- ProgressBar.jsx
+++ ProgressBar.jsx
@@ -6,7 +6,10 @@ const ProgressBar = ({ currentStep, totalSteps = 5 }) => {
         <div className="progress-bar">
             <div className="line"></div>
             {Array.from({ length: totalSteps }).map((_, index) => (
-                <div key={index} className={`dot ${index < currentStep ? 'completed' : ''}`}></div>
+                <div 
+                    key={index} 
+                    className={`dot ${index < currentStep ? 'completed' : ''} ${index === currentStep - 1 ? 'current' : ''}`}
+                ></div>
             ))}
         </div>
     );
