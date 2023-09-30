10e8732eb08f1124ec55513e7c29b3bd47146842
Andrea Solis <acsolis896@gmail.com>
2023-09-30 01:19:44 -0400

    progress bar

--- ProgressBar.jsx
+++ ProgressBar.jsx
@@ -5,10 +5,10 @@ const ProgressBar = ({ currentStep, totalSteps = 5 }) => {
     return (
         <div className="progress-bar">
             <div className="line"></div>
-            {Array.from({ length: totalSteps }).map((_, index) => (
+            {Array.from({ length: totalSteps + 1 }).map((_, index) => (
                 <div 
                     key={index} 
-                    className={`dot ${index < currentStep ? 'completed' : ''} ${index === currentStep - 1 ? 'current' : ''}`}
+                    className={`dot ${index === 0 || index < currentStep + 1 ? 'completed' : ''} ${index === currentStep ? 'current' : ''}`}
                 ></div>
             ))}
         </div>
