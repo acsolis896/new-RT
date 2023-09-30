1e96b567457796d25d3a9bdb76e7c4cdb75a4ee2
Andrea Solis <acsolis896@gmail.com>
2023-09-30 17:14:35 -0400

    css

--- TvSelection.jsx
+++ TvSelection.jsx
@@ -65,7 +65,7 @@ function TvSelection() {
 
       <div className="page-width-medium">        
           <h2 style={{marginTop:'30px', marginBottom: '10px', fontSize: '35px'}}>Select your top 5 TV shows</h2>
-          <p style={{marginBottom: '10px', fontWeight: 'normal', fontSize: '15px'}}>Selecting your top 5 TV-shows will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
+          <p style={{marginBottom: '10px', marginRight: '10%', marginLeft: '10%', fontWeight: 'normal', fontSize: '15px'}}>Selecting your top 5 TV-shows will enable us to suggest like-minded users and nearby communities for exciting watch parties and movie premiere gatherings.</p>
 
           <div className="search-bar-div">
             <i class="fa-solid fa-magnifying-glass"></i>
